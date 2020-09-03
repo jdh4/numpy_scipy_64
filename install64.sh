04fac357b20ba8609726b8ab6f73f47771a7dc03
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-09-03 16:59:22 -0400

    Update install64.sh

--- install64.sh
+++ install64.sh
@@ -20,12 +20,11 @@ cp site.cfg.example site.cfg
 rh8
 wget https://raw.githubusercontent.com/jdh4/numpy_scipy_64/master/numpy/site.cfg
 ./pip3 install --user cython
-/home/jdh4/software/Python-3.8.5/exec/bin/python3 setup.py build
-/home/jdh4/software/Python-3.8.5/exec/bin/python3 setup.py install --prefix=/home/jdh4/.local
-/home/jdh4/software/Python-3.8.5/exec/bin/python3
+$HOME/software/Python-3.8.5/exec/bin/python3 setup.py install --prefix=$HOME/.local
+$HOME/software/Python-3.8.5/exec/bin/python3
 
 wget https://github.com/scipy/scipy/releases/download/v1.5.2/scipy-1.5.2.tar.gz
 tar zxf scipy-1.5.2.tar.gz
 cd scipy-1.5.2
 wget https://raw.githubusercontent.com/jdh4/numpy_scipy_64/master/scipy/site.cfg
-/home/jdh4/software/Python-3.8.5/exec/bin/python3 setup.py install --prefix=/home/jdh4/.local
+$HOME/software/Python-3.8.5/exec/bin/python3 setup.py install --prefix=$HOME/.local
