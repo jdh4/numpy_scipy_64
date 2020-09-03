6a228988a5689c4a2517aebea94189cbcd047c93
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-09-03 17:09:03 -0400

    Update install64.sh

--- install64.sh
+++ install64.sh
@@ -1,5 +1,7 @@
 module purge
 
+## build Python ##
+cd $HOME/software
 wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tgz
 tar zxf Python-3.8.5.tgz
 cd Python-3.8.5
@@ -10,21 +12,27 @@ module load rh/devtoolset/8
 make -j 10
 make test  # some tests fail
 make install
-cd exec/bin
-./python3
+./exec/bin/python3 --version
 
 export NPY_USE_BLAS_ILP64=1
 export NPY_BLAS_ILP64_ORDER=openblas_ilp64
 export NPY_LAPACK_ILP64_ORDER=openblas_ilp64
 
-git clone https://github.com/numpy/numpy.git
+## build numpy against 64-bit openblas ##
+cd $HOME/software
+wget https://github.com/numpy/numpy/archive/v1.19.1.tar.gz
+tar zxf v1.19.1.tar.gz
+cd numpy-1.19.1
 wget https://raw.githubusercontent.com/jdh4/numpy_scipy_64/master/numpy/site.cfg
-./pip3 install --user cython
+$HOME/software/Python-3.8.5/exec/bin/pip3 install --user cython
 $HOME/software/Python-3.8.5/exec/bin/python3 setup.py install --prefix=$HOME/.local
-$HOME/software/Python-3.8.5/exec/bin/python3
+$HOME/software/Python-3.8.5/exec/bin/python3 -c "import numpy; numpy.show_config()"
 
+## build scipy against 64-bit openblas ##
+cd $HOME/software
 wget https://github.com/scipy/scipy/releases/download/v1.5.2/scipy-1.5.2.tar.gz
 tar zxf scipy-1.5.2.tar.gz
 cd scipy-1.5.2
 wget https://raw.githubusercontent.com/jdh4/numpy_scipy_64/master/scipy/site.cfg
 $HOME/software/Python-3.8.5/exec/bin/python3 setup.py install --prefix=$HOME/.local
+$HOME/software/Python-3.8.5/exec/bin/python3 -c "import scipy; scipy.show_config()"
